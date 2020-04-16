ca58435abb5c6b6b736ce58fa3dcc77aa0f1ac4d
tongwanming <1623644929@qq.com>
2020-04-16 16:39:56 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/Pods-XMpodDemo-Tong_Example/Pods-XMpodDemo-Tong_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XMpodDemo_Tong_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XMpodDemo_Tong_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-XMpodDemo-Tong_Tests/Pods-XMpodDemo-Tong_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XMpodDemo_Tong_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XMpodDemo_Tong_Tests
+@end
